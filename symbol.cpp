e23cde19a78ff03730a0f1dcc5ef1a27a5410934
Marcial Gaißert <marcial.gaissert@gmail.com>
2014-10-23 18:10:54 +0200

    Add possibility for creating uninterned symbols.

--- symbol.cpp
+++ symbol.cpp
@@ -1,17 +1,23 @@
 #include "symbol.h"
 
-idifiles::Symbol::Symbol(std::string name){
+idifiles::Symbol::Symbol(std::string name, bool intern){
   this->name=name;
-  symboltable.insert(std::make_pair<std::string, Symbol* const>
-		     (name,this));
+  if(intern)
+    symboltable.insert(std::make_pair<std::string, Symbol* const>
+		       (name,this));
 }
 
-Symbol* const idifiles::Symbol::intern(std::string name){
+idifiles::Symbol* const idifiles::Symbol::intern(std::string name){
   if(symboltable.find(name)!=symboltable.end())
     return symboltable[name];
   else{
     return new Symbol(name);
   }
+}
+
+idifiles::Symbol* const idifiles::Symbol::makeUninternedSymbol
+(std::string name){
+  return 
 }
 
 idifiles::Symbol::~Symbol(){}
