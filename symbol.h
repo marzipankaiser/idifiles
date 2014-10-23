8e4694ed4b772d952a26f8af7d705dfd72011bd0
Marcial Gaißert <marcial.gaissert@gmail.com>
2014-10-23 18:09:44 +0200

    (Empty description)

--- symbol.h
+++ symbol.h
@@ -10,9 +10,10 @@ namespace idifiles{
     const std::string name;
     static std::unordered_map<std::string, Symbol* const> symboltable;
 
-    Symbol(std::string name);
+    Symbol(std::string name, bool intern=true);
   public:
     static Symbol* const intern(std::string name);
+    static Symbol* const makeUninternedSymbol(std::string name);
     virtual ~Symbol();
   };
 
