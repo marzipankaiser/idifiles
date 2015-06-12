681ce61723bb0e8f7f27b0272289885715ae468a
marzipankaiser <marzipankaiser@gmail.com>
2015-06-12 19:02:01 +0200

    Add environment

---
+++ environment.cpp
@@ -0,0 +1,27 @@
+#include "environment.h"
+
+using namespace idifiles;
+using namespace std;
+
+bool environment::is_bound(symbol* s){
+  if(local_bindings.count(s)>0) return true;
+  else if(parent) return parent->is_bound();
+  else return false;
+}
+object* environment::get(symbol* s){
+  auto it = local_bindings.find(s);
+  if(it!=local_bindings.end())
+    return it->second;
+  else if(parent) return parent->get(s);
+  else return s->global_value;
+}
+void environment::set(symbol* s, object* v){
+  auto it = local_bindings.find(s);
+  if(it!=local_bindings.end())
+    it->second = v;
+  else if(parent) parent->set(s, v);
+  else s->global_value = v;
+}
+void environment::bind(symbol* s, object* v){
+  local_bindings[s] = v;
+}
---
+++ environment.h
@@ -0,0 +1,25 @@
+#ifndef IDIFILES_ENVIRONMENT_H
+#define IDIFILES_ENVIRONMENT_H
+
+#include <map>
+#include "symbol.h"
+#include "object.h"
+
+namespace idifiles{
+
+  class environment{
+
+    environment* parent;
+    std::map<symbol*, object*> local_bindings;
+
+  public:
+    bool is_bound();
+    object* get(symbol* s);
+    void set(symbol* s, object* v);
+    void bind(symbol* s, object* v);
+    
+  };
+  
+}
+
+#endif
