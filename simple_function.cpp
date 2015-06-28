c397b9df4ad9b0d54c3d14e3739aebe377e8abc3
marzipankaiser <marzipankaiser@gmail.com>
2015-06-28 17:22:41 +0200

    Stub for simple_function.cpp

---
+++ simple_function.cpp
@@ -0,0 +1,9 @@
+#include "simple_function.h"
+
+using namespace idifiles;
+
+template<class R, class... Ts>
+object* simple_function<R,Ts...>::funcall
+(std::map<symbol*, object*> arg){
+  //TODO
+}
