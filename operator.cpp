ee54ef94a25166ca0ad67d43508bb23053cd9b3c
marzipankaiser <marzipankaiser@gmail.com>
2015-07-13 17:10:24 +0200

    Cleanup. Start rewrite. Again...

--- operator.cpp
+++
@@ -1,5 +0,0 @@
-#include "operator.h"
-
-using namespace idifiles;
-
-void Operator::map_ptrs(std::function<void(object*)> fn){}
--- simple_function.cpp
+++
@@ -1,9 +0,0 @@
-#include "simple_function.h"
-
-using namespace idifiles;
-
-template<class R, class... Ts>
-object* simple_function<R,Ts...>::funcall
-(std::map<symbol*, object*> arg){
-  //TODO
-}
